06096126245c9337c4d3616277f02f8fb4e9634c
Trevor Mast <trevormast@gmail.com>
2021-09-15 06:40:47 -0700

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'users/loggable_items#index'
+
+  # workaround for issue when there is a registration error--rails automagically
+  # redirects to the user index?
+  get '/users', to: redirect('/users/sign_up')
+
+  resources :users, only: [:show, :edit, :update]  do
+    resources :loggable_items, only: [:index, :new, :create] do
+      resources :logs, only: [:index, :create]
+    end
+  end
 end
